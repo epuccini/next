ed33e4843431bb6be7e91a6d14fd1af7f48fc896
Edward Puccini <edward@Edwards-MacBook-Air.local>
2019-08-14 16:03:34 +0200

    Add file

---
+++ src/file.lisp
@@ -0,0 +1,29 @@
+; -------------------------------------------------------------
+; Edward Alan Puccini 24.05.2013
+; -------------------------------------------------------------
+; Feedforward Artificial Neural Network lib and main program.
+; with backpropagation learning-, offline algorithm 
+; -------------------------------------------------------------
+; nn-io.lisp - Input, output data
+; Function load and save to file, to add data to a stack
+; -------------------------------------------------------------
+
+(in-package :next)
+
+(defun load-data (path)
+  "Load ascii file from path."
+  (let ((store))
+	(with-open-file (stream path 
+				:direction :input)
+	  (with-standard-io-syntax
+		(setf store (read stream))))
+	store))
+
+(defun save-data (path store)
+  "Save ascii data to path from store."
+  (with-open-file (stream path 
+			  :direction :output
+			  :if-exists :supersede)
+    (with-standard-io-syntax
+	  (print store stream))))
+
