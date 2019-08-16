da77a23b0cf435f082beadc7ced6778509c896f1
Edward.Puccini <Edward.Puccini@partner.bmw.de>
2019-08-16 10:30:29 +0200

    Remove deprecated

--- src/file.lisp
+++ src/file.lisp
@@ -30,22 +30,3 @@
          (cl-binary:write-u8 out (char-code (elt data x))))))
 
 
-(defun load-data (path)
-  "Load ascii file from path."
-  (let ((store))
-	(with-open-file (stream path 
-				            :direction :input
-                            :external-format :utf-8)
-	  (with-standard-io-syntax
-		(setf store (read stream))))
-	store))
-
-(defun save-data (path store)
-  "Save ascii data to path from store."
-  (with-open-file (stream path 
-			              :direction :output
-                          :external-format :utf-8
-			              :if-exists :supersede)
-    (with-standard-io-syntax
-	  (print store stream))))
-
