296f82ad21744b767af58ac949178bfaff998267
Edward Puccini <edward@Edwards-MacBook-Air.local>
2019-08-27 19:04:49 +0200

    Implemented pointer to array functions

--- output/output.c
+++ output/output.c
@@ -88,12 +88,18 @@ void print_format(const char* fmt, ...) {
     for(p = fmt; *p != '\0'; p++) {
         if (*p == '%') {
             p++;
-            if (*p == 'd')
-                printf("%d", va_arg(arglist, int));
-            if (*p == 's')
-                printf("%s", va_arg(arglist, char*));
-            if (*p == 'c')
-                printf("%c", va_arg(arglist, int));
+            if (*p == 'd') {
+                int val = va_arg(arglist, int);
+                printf("%d", val);
+            }
+            if (*p == 's') {
+                char* val = va_arg(arglist, char*);
+                printf("%s", val);
+            }
+             if (*p == 'c') {
+                 int val = va_arg(arglist, int);
+                 printf("%c", val);
+            }
             continue;
         }
     };
@@ -101,36 +107,68 @@ void print_format(const char* fmt, ...) {
     printf("\n");
 }
 
-bool elt_bool(bool* ptr, int idx) {
-    return (bool)ptr[idx];
+bool* elt_bool(bool* ptr, int idx) {
+    return (bool*)&ptr[idx];
 }
 
-char elt_byte(char* ptr, int idx) {
-    return (char)ptr[idx];
+char* elt_byte(char* ptr, int idx) {
+    return (char*)&ptr[idx];
 }
 
-short elt_int16(short* ptr, int idx) {
-    return (short)ptr[idx];
+short* elt_int16(short* ptr, int idx) {
+    return (short*)&ptr[idx];
 }
 
-int elt_int32(int* ptr, int idx) {
-    return (int)ptr[idx];
+int* elt_int32(int* ptr, int idx) {
+    return (int*)&ptr[idx];
 }
 
-long elt_int64(long* ptr, short idx) {
-    return (long)ptr[idx];
+long* elt_int64(long* ptr, short idx) {
+    return (long*)&ptr[idx];
 }
 
-float elt_float32(float* ptr, short idx) {
-    return (float)ptr[idx];
+float* elt_float32(float* ptr, short idx) {
+    return (float*)&ptr[idx];
 }
 
-double elt_float64(double* ptr, int idx) {
-    return (double)ptr[idx];
+double* elt_float64(double* ptr, int idx) {
+    return (double*)&ptr[idx];
 }
 
-char* elt_string(char** ptr, int idx) {
-    return (char*)ptr[idx];
+char** elt_string(char** ptr, int idx) {
+    return (char**)&ptr[idx];
+}
+
+void set_bool(bool* ptr, bool val) {
+    *ptr = val;
+}
+
+void set_byte(char* ptr, char val) {
+    *ptr= val;
+}
+
+void set_int16(short* ptr, short val) {
+    *ptr= val;
+}
+
+void set_int32(int* ptr, int val) {
+    *ptr = val;
+}
+
+void set_int64(long* ptr, long val) {
+    *ptr = val;
+}
+
+void set_float32(float* ptr, float val) {
+    *ptr = val;
+}
+
+void set_float64(double* ptr, double val) {
+    *ptr = val;
+}
+
+void set_string(char** ptr, char* val) {
+    *ptr = val;
 }
 
 short add_byte(int size, ...) {
@@ -212,14 +250,15 @@ int do_1(int argc_1)
 float float1_2=0.0;
 float float2_2=0.0;
 int array_2[]={1, 2, 3, 4, 5, 6};
-float1_2=(666.0);
-argc_1=(add_int32(3,100,1000,500));
-argc_1=(add_int32(2,100,add_int32(2,1000,999)));
+set_float32(&float1_2,666.0);
+set_int32(&argc_1,add_int32(3,100,1000,500));
+set_int32(&argc_1,add_int32(2,100,add_int32(2,1000,999)));
 int cnt_3=0;
 for(cnt_3=0;cnt_3<5;cnt_3++)
 {
+set_int32(elt_int32(array_2,cnt_3),1000);
 print_str("ELT:");
-print_format("%d",elt_int32(array_2,cnt_3));
+print_format("%d",*elt_int32(array_2,cnt_3));
 print_str("CNT:");
 println_int32(cnt_3);
 }
--- templates/code.c
+++ templates/code.c
@@ -88,12 +88,18 @@ void print_format(const char* fmt, ...) {
     for(p = fmt; *p != '\0'; p++) {
         if (*p == '%') {
             p++;
-            if (*p == 'd')
-                printf("%d", va_arg(arglist, int));
-            if (*p == 's')
-                printf("%s", va_arg(arglist, char*));
-            if (*p == 'c')
-                printf("%c", va_arg(arglist, int));
+            if (*p == 'd') {
+                int val = va_arg(arglist, int);
+                printf("%d", val);
+            }
+            if (*p == 's') {
+                char* val = va_arg(arglist, char*);
+                printf("%s", val);
+            }
+             if (*p == 'c') {
+                 int val = va_arg(arglist, int);
+                 printf("%c", val);
+            }
             continue;
         }
     };
@@ -101,36 +107,68 @@ void print_format(const char* fmt, ...) {
     printf("\n");
 }
 
-bool elt_bool(bool* ptr, int idx) {
-    return (bool)ptr[idx];
+bool* elt_bool(bool* ptr, int idx) {
+    return (bool*)&ptr[idx];
 }
 
-char elt_byte(char* ptr, int idx) {
-    return (char)ptr[idx];
+char* elt_byte(char* ptr, int idx) {
+    return (char*)&ptr[idx];
 }
 
-short elt_int16(short* ptr, int idx) {
-    return (short)ptr[idx];
+short* elt_int16(short* ptr, int idx) {
+    return (short*)&ptr[idx];
 }
 
-int elt_int32(int* ptr, int idx) {
-    return (int)ptr[idx];
+int* elt_int32(int* ptr, int idx) {
+    return (int*)&ptr[idx];
 }
 
-long elt_int64(long* ptr, short idx) {
-    return (long)ptr[idx];
+long* elt_int64(long* ptr, short idx) {
+    return (long*)&ptr[idx];
 }
 
-float elt_float32(float* ptr, short idx) {
-    return (float)ptr[idx];
+float* elt_float32(float* ptr, short idx) {
+    return (float*)&ptr[idx];
 }
 
-double elt_float64(double* ptr, int idx) {
-    return (double)ptr[idx];
+double* elt_float64(double* ptr, int idx) {
+    return (double*)&ptr[idx];
 }
 
-char* elt_string(char** ptr, int idx) {
-    return (char*)ptr[idx];
+char** elt_string(char** ptr, int idx) {
+    return (char**)&ptr[idx];
+}
+
+void set_bool(bool* ptr, bool val) {
+    *ptr = val;
+}
+
+void set_byte(char* ptr, char val) {
+    *ptr= val;
+}
+
+void set_int16(short* ptr, short val) {
+    *ptr= val;
+}
+
+void set_int32(int* ptr, int val) {
+    *ptr = val;
+}
+
+void set_int64(long* ptr, long val) {
+    *ptr = val;
+}
+
+void set_float32(float* ptr, float val) {
+    *ptr = val;
+}
+
+void set_float64(double* ptr, double val) {
+    *ptr = val;
+}
+
+void set_string(char** ptr, char* val) {
+    *ptr = val;
 }
 
 short add_byte(int size, ...) {
