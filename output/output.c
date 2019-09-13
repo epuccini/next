19d9eb1a2258c5d64fd83860eadb394072f89f6b
Edward Puccini <edward@Edwards-MacBook-Air.local>
2019-09-13 20:12:13 +0200

    Working half

--- output/output.c
+++ output/output.c
@@ -4,11 +4,11 @@
 static f64 acc_0=0;
 static f64 den_0=1;
 static f64 num_0=1;
-f64 extract_digit_0(i64 nth_1)
+f64 extract_digit_0(ui64 nth_1)
 {
 return(trunc((((num_0*nth_1)+acc_0)/den_0)));
 }
-void eliminate_digit_0(f64 d_1)
+void eliminate_digit_0(ui64 d_1)
 {
 set_pointer_f64(&acc_0,(acc_0-(den_0*d_1)));
 set_pointer_f64(&acc_0,(acc_0*10));
@@ -27,30 +27,30 @@ set_pointer_f64(&num_0,(num_0*k_1));
 i32 main(i32 argc_1,string* argv_1)
 {
 {
-f64 d_2=0;
-f64 k_2=0;
-i64 i_2=0;
-i64 n_2=10000;
-set_pointer_i64(&n_2,10000);
+ui64 d_2=0;
+ui64 k_2=0;
+ui64 i_2=0;
+ui64 n_2=10000;
+set_pointer_ui64(&n_2,10000);
 while(i_2<n_2)
 {
-set_pointer_f64(&k_2,(k_2+1));
+set_pointer_ui64(&k_2,(k_2+1));
 next_term_0(k_2);
 if(num_0>acc_0)
 {
 continue;
 }
-set_pointer_f64(&d_2,extract_digit_0(3));
+set_pointer_ui64(&d_2,extract_digit_0(3));
 if(d_2!=extract_digit_0(4))
 {
 continue;
 }
-print_f64(d_2);
+print_ui64(d_2);
 print_string(" ");
-set_pointer_i64(&i_2,(1+i_2));
+set_pointer_ui64(&i_2,(1+i_2));
 if(mod(i_2,10)==0)
 {
-println_i64(i_2);
+println_ui64(i_2);
 }
 eliminate_digit_0(d_2);
 };
